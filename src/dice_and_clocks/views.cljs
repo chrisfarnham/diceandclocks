ed5a1fd7a0cf38cab30cb3048771ad72d0a55fcf
Chris Farnham <cfarnham@artisight.com>
2026-09-14 17:41:06 -0400

    feat(analytics): track PNG export usage
    Every other user action (roll-dice, send-message, create-clock,
    enter-channel) is tracked via analytics/log-event; PNG export was the
    one exception. Threaded context through to clocks-to-png and added
    :export-clocks-png, following the same {:channel-id :name} shape as
    every other tracked event. Inherits the existing dev/preview-channel
    analytics gating (config/track-analytics?) automatically.

--- src/dice_and_clocks/views.cljs
+++ src/dice_and_clocks/views.cljs
@@ -353,11 +353,12 @@
 )
 (def to-png-options (clj->js {:filter clocks-to-png-filter}))
 
-(defn clocks-to-png []
+(defn clocks-to-png [context]
 (let [clock-panel-div (. js/document (getElementById "clock-panel"))]
+  (analytics/log-event :export-clocks-png {:channel-id (:channel context) :name (:name context)})
   (-> clock-panel-div
       (html-to-image/toPng to-png-options)
-      (.then 
+      (.then
        (fn [data-url] (downloadjs data-url "clocks.png")))
   )
 ))
@@ -380,7 +381,7 @@
     (when (< 0 (count clocks))
       [:div {:class "p-2"}
       [:a {:class "text-sm text-center print:hidden" :href "#"
-           :on-click #(clocks-to-png)}
+           :on-click #(clocks-to-png context)}
        [:i {:class "fas fa-camera"}]]])
     ]
   ]]]
